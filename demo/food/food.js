c8d5e73b5311265b6663308d118a045cb668fc87
Max131 <max_cable@hotmail.com>
2020-04-13 02:33:43 -0500

    Added food demo

---
+++ demo/food/food.js
@@ -0,0 +1,23 @@
+bulmaCarousel.attach('.hero-carousel', {
+    navigationKeys: true,
+    loop: true,
+    infinite: true,
+    autoplay: true,
+    pauseOnHover: false,
+    autoplaySpeed: 5000,
+    slidesToShow: 1
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    if ($navbarBurgers.length > 0) {
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+        });
+      });
+    }
+  });
